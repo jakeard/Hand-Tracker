d84b3df1ec65b892840bc088bf8145f4dc22491e
Jake Ard <ard20001@byui.edu>
2022-10-08 19:58:08 -0600

    tracking landmarks

--- Track/__main__.py
+++ Track/__main__.py
@@ -2,7 +2,24 @@ import cv2
 import mediapipe as mp
 
 def main():
-    pass
+    cap = cv2.VideoCapture(0)
+
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    mp_draw = mp.solutions.drawing_utils
+
+    while True:
+        success, img = cap.read()
+        img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(img_RGB)
+
+        print(results.multi_hand_landmarks)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_draw.draw_landmarks(img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
 if __name__ == '__main__':
     main()
