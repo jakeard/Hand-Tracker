3714cc037e918d135592865dcd86fcd9d5d9ecd3
Jake Ard <ard20001@byui.edu>
2022-11-01 22:45:55 -0600

    mouse movement more efficient

--- Track/logic/director.py
+++ Track/logic/director.py
@@ -2,6 +2,9 @@ import cv2
 import mediapipe as mp
 import time
 import numpy as np
+import pyautogui
+import pydirectinput as pdi
+import autoit
 from logic.handle_action import Handle_Action
 from logic.finger import Finger
 
@@ -17,6 +20,9 @@ class Director:
     
     def run(self):
         cap = cv2.VideoCapture(0)
+        cap.set(3, 640)
+        cap.set(4, 480)
+        # (screen_w, screen_h) = pdi.size()
         mp_hands = mp.solutions.hands
         hands = mp_hands.Hands()
         mp_draw = mp.solutions.drawing_utils
@@ -28,6 +34,9 @@ class Director:
             results = hands.process(img_RGB)
 
             if results.multi_hand_landmarks:
+                curr_hand = []
+                for hand in results.multi_handedness:
+                    curr_hand.append(hand.classification[0].label)
                 for hand_lmks in results.multi_hand_landmarks:
                     tracker = {}
                     count = -1
@@ -43,7 +52,7 @@ class Director:
                         #     print(f'x: {lm_x}, y: {lm_y}')
                     mp_draw.draw_landmarks(img, hand_lmks, mp_hands.HAND_CONNECTIONS, mp_draw.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2), mp_draw.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2))
                 self.set_finger_loc(tracker)
-                self.actions.check()
+                self.actions.check(curr_hand)
 
             cv2.imshow("Image", img)
             cv2.waitKey(1)
