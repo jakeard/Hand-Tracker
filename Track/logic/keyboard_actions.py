022ae8b510905c38beefa9748b560bae13cf7a81
Jake Ard <ard20001@byui.edu>
2022-11-12 10:32:28 -0700

    keyboard updates

--- Track/logic/keyboard_actions.py
+++ Track/logic/keyboard_actions.py
@@ -1,4 +1,6 @@
 import autoit
+from pynput.keyboard import Key, Controller
+import time
 
 class Keyboard_Actions:
     def __init__(self, fingers):
@@ -8,17 +10,64 @@ class Keyboard_Actions:
         self._middle = fingers[2]
         self._ring = fingers[3]
         self._pinky = fingers[4]
-        self._prev_pros = (0, 0)
+        # self._prev_pros = (0, 0)
+        self._pressed = 'z'
+        # self._press = None
+        # self._currently_pressed = None
+        self._keyboard = Controller()
     
     def check_match(self, open):
+        press = None
         if open == ['pointer']:
-            autoit.send('W')
-        if open == ['pointer', 'middle']:
-            autoit.send('A')
-        if open == ['pointer', 'middle', 'ring']:
-            autoit.send('S')
-        if open == ['pointer', 'middle', 'ring', 'pinky']:
-            autoit.send('D')
+            press = 'w'
+            # self._keyboard.release(self._pressed)
+            # self._keyboard.press('w')
+            # self._pressed = 'w'
+            # self._keyboard.release('w')
+            # autoit.send('W')
+        elif open == ['pointer', 'middle']:
+            press = 'a'
+            # self._press_key('a')
+            # self._keyboard.release(self._pressed)
+            # self._keyboard.press('a')
+            # self._keyboard.press('w')
+            # self._pressed = 'a'
+            # autoit.send('A')
+        elif open == ['pointer', 'middle', 'ring']:
+            press = 's'
+            # self._press_key('s')
+            # self._keyboard.press('w')
+            # self._keyboard.release(self._pressed)
+            # self._keyboard.press('s')
+            # self._pressed = 's'
+            # autoit.send('S')
+        elif open == ['pointer', 'middle', 'ring', 'pinky']:
+            # print('AHHHHHHHHHHHHHHHHHHHHHHHHH')
+            press = 'd'
+        # else:
+            # press = None
+            # self._press_key('d')
+            # self._keyboard.release(self._pressed)
+            # self._keyboard.press('d')
+            # self._pressed = 'd'
+            # autoit.send('D')
+        # else:
+            # self._keyboard.release(self._pressed)
+        self._press_key(press)
+        # autoit.send()
         # self._check_w(open)
+    
+    # def _is_pressed(self, key):
+    #     if self._pressed != key:
+    #         return True
+    #     return False
 
+    def _press_key(self, key):
+        # self._keyboard.release(self._pressed)
+        # if key is not None:
+        if key != self._pressed:
+            self._keyboard.release(key)
+        if key is not None:
+            self._keyboard.press(key)
+        self._pressed = key
     # def _check_w(self, open):
