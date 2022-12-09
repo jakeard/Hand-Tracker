eccd137340972b478043e4b3b157fc1bc3014980
Jake Ard <ard20001@byui.edu>
2022-12-08 19:17:35 -0700

    more GUI options, added click and hold, removed unnecessary comments

--- Track/__main__.py
+++ Track/__main__.py
@@ -1,6 +1,7 @@
 from logic.director import Director
 import tkinter as tk
 from tkinter import ttk
+import sys
 
 
 def _p1(): 
@@ -11,14 +12,18 @@ def _p2():
 # Function to make sure ASL can be used
     _begin(Director('asl'))
 
+def _p3():
+    sys.exit()
+
 def _begin(start): 
 # Function to take the info from either _p1 or _p2 and start the actual program
-    root.quit()
+    root.destroy()
     start.run()
 
 
-if __name__ == '__main__': # If this is the file the program is started in, begin
-    root = tk.Tk()         # Setting up the GUI
+if __name__ == '__main__': # Start here
+    x = 1
+    root = tk.Tk()   # Setting up the GUI
     root.geometry('300x200')
     root.resizable(False, False)
     root.title('Selection')
@@ -44,6 +49,19 @@ if __name__ == '__main__': # If this is the file the program is started in, begi
     )
 
     p2_button.pack(
+        ipadx=5,
+        ipady=5,
+        expand=True
+    )
+
+    # Button to end the program
+    p3_button = ttk.Button(
+        root,
+        text='Quit',
+        command=_p3
+    )
+
+    p3_button.pack(
         ipadx=5,
         ipady=5,
         expand=True
--- Track/logic/keyboard_actions.py
+++ Track/logic/keyboard_actions.py
@@ -1,6 +1,5 @@
 import pyautogui
 import pydirectinput as pdi
-import time
 
 class Keyboard_Actions:
     """
@@ -18,56 +17,19 @@ class Keyboard_Actions:
         self._middle = fingers[2]
         self._ring = fingers[3]
         self._pinky = fingers[4]
-        # self._prev_pros = (0, 0)
         self._pressed = None
-        # self._press = None
-        # self._currently_pressed = None
-        # self._keyboard = Controller()
     
     def check_match(self, open):
     # Compares open fingers to what is needed to do a specific action
         press = None
         if open == ['pointer']:
             self._press_key('w')
-            # press = 'w'
-            # self._keyboard.release(self._pressed)
-            # self._keyboard.press('w')
-            # self._pressed = 'w'
-            # self._keyboard.release('w')
-            # autoit.send('W')
         elif open == ['pointer', 'middle']:
             self._press_key('a')
-            # press = 'a'
-            # self._press_key('a')
-            # self._keyboard.release(self._pressed)
-            # self._keyboard.press('a')
-            # self._keyboard.press('w')
-            # self._pressed = 'a'
-            # autoit.send('A')
         elif open == ['pointer', 'middle', 'ring']:
             self._press_key('s')
-            # press = 's'
-            # self._press_key('s')
-            # self._keyboard.press('w')
-            # self._keyboard.release(self._pressed)
-            # self._keyboard.press('s')
-            # self._pressed = 's'
-            # autoit.send('S')
         elif open == ['pointer', 'middle', 'ring', 'pinky']:
-            # print('AHHHHHHHHHHHHHHHHHHHHHHHHH')
             self._press_key('d')
-            # press = 'd'
-        # else:
-            # press = None
-            # self._press_key('d')
-            # self._keyboard.release(self._pressed)
-            # self._keyboard.press('d')
-            # self._pressed = 'd'
-            # autoit.send('D')
-        # else:
-            # self._keyboard.release(self._pressed)
-        # autoit.send()
-        # self._check_w(open)
 
     def _press_key(self, key):
     # Controls key presses and releases
